27507815d60b39fbe04b95b91f657bab689a6638
Talon7603 <livehoperun@gmail.com>
2025-06-15 14:39:23 -0400

    Update urls.py

--- employee_project/urls.py
+++ employee_project/urls.py
@@ -10,6 +10,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from employees.views import chart_view
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -42,3 +44,5 @@ schema_view = get_schema_view(
 urlpatterns += [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+urlpatterns += [path("charts/", chart_view, name="charts")]
