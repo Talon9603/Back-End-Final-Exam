6dbfe216fe2aff86b0bf81fac5c6d946401be402
Talon7603 <livehoperun@gmail.com>
2025-06-15 14:29:27 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,3 +25,20 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+}
