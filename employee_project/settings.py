9cea27fd680016b6d5703f58410003fffb110953
Talon7603 <livehoperun@gmail.com>
2025-06-16 13:27:03 -0400

    Update settings.py

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -25,6 +25,17 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'employees',
+    'departments',
+    'attendance',
+    'performance',
+    'drf_yasg',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 
