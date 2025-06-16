1fc4640d088bc1cc1470ee3d3da0617c805cbc31
Talon7603 <livehoperun@gmail.com>
2025-06-16 14:06:38 -0400

    Update urls.py

--- employee_project/urls.py
+++ employee_project/urls.py
@@ -17,19 +17,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from django.shortcuts import redirect 
+
 router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet)
 router.register(r'departments', DepartmentViewSet)
 router.register(r'attendance', AttendanceViewSet)
 router.register(r'performance', PerformanceViewSet)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -41,8 +35,12 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns += [
+urlpatterns = [
+    path('', lambda request: redirect('/swagger/')),  
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('charts/', chart_view, name='charts'),
 ]
-
-urlpatterns += [path("charts/", chart_view, name="charts")]
